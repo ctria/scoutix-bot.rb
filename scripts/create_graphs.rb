24c5e8d1b10aa747607a542755367d6d46f99394
Christos Triantafyllidis <christos.triantafyllidis@gmail.com>
2015-10-19 00:59:18 +0100

    Updated dates for JOTI 2015

--- scripts/create_graphs.rb
+++ scripts/create_graphs.rb
@@ -4,11 +4,34 @@ require 'sqlite3'
 require 'gruff'
 require 'time'
 
-StartOfJOTI = "2013-10-18 18:00:00 +0300"
-EndOfJOTI = "2013-10-21 03:00:00 +0300"
-Channel = "#greek"
+StartOfJOTI = "2015-10-16 15:00:00 +0300"
+EndOfJOTI = "2015-10-19 03:00:00 +0300"
+Channel = "#" + ARGV[0]
 TimeStep = 1800
-OutputDir = "var/graphs"
+OutputDir = "var/graphs/" + ARGV[0]
+
+TimeLabels = {
+  1445007600 => "Fri 18:00",
+#  1445018400 => "Fri 21:00",
+  1445029200 => "Sat 00:00",
+#  1445040000 => "Sat 03:00",
+  1445050800 => "Sat 06:00",
+#  1445061600 => "Sat 09:00",
+  1445072400 => "Sat 12:00",
+#  1445083200 => "Sat 15:00",
+  1445094000 => "Sat 18:00",
+#  1445104800 => "Sat 21:00",
+  1445115600 => "Sun 00:00",
+#  1445126400 => "Sun 03:00",
+  1445137200 => "Sun 06:00",
+#  1445148000 => "Sun 09:00",
+  1445158800 => "Sun 12:00",
+#  1445169600 => "Sun 15:00",
+  1445180400 => "Sun 18:00",
+#  1445191200 => "Sun 21:00",
+  1445202000 => "Mon 00:00",
+#  1445212800 => "Mon 03:00",
+}
 
 db = SQLite3::Database.open('var/channel_members.db')
 datasets = db.execute('SELECT time,count FROM ChannelMembers WHERE Channel = ? AND time >= ? AND time <= ? ORDER BY time', Channel, Time.parse(StartOfJOTI).to_s, Time.parse(EndOfJOTI).to_s)
@@ -22,26 +45,10 @@ db.close
 
 g = Gruff::Line.new
 g.dataxy("Members in the #{Channel} channel",datasets.collect{|data| Time.parse(data[0]).to_i},datasets.collect{|data| data[1]})
+g.marker_font_size = 13
+g.labels = TimeLabels
+g.hide_dots =true
 
-g.labels = {
-  1382130000 => "Sat 00:00",
-#  1382140800 => "Sat 03:00",
-#  1382151600 => "Sat 06:00",
-#  1382162400 => "Sat 09:00",
-  1382173200 => "Sat 12:00",
-#  1382184000 => "Sat 15:00",
-#  1382194800 => "Sat 18:00",
-#  1382205600 => "Sat 21:00",
-  1382216400 => "Sun 00:00",
-#  1382227200 => "Sun 03:00",
-#  1382238000 => "Sun 06:00",
-#  1382248800 => "Sun 09:00",
-  1382259600 => "Sun 12:00",
-#  1382270400 => "Sun 15:00",
-#  1382281200 => "Sun 18:00",
-#  1382292000 => "Sun 21:00",
-  1382302800 => "Mon 00:00",
-}
 g.write(OutputDir + '/channel_members.png')
 
 g = Gruff::Pie.new
@@ -77,23 +84,7 @@ g = Gruff::Line.new
 g.title = "#{prvmsgs.count} messages in the channel"
 g.hide_dots =true
 g.dataxy("Messages sent in the #{Channel} channel per 30 minutes",datasets.collect{|data| data[0]},datasets.collect{|data| data[1]})
-g.labels = {
-  1382130000 => "Sat 00:00",
-#  1382140800 => "Sat 03:00",
-#  1382151600 => "Sat 06:00",
-#  1382162400 => "Sat 09:00",
-  1382173200 => "Sat 12:00",
-#  1382184000 => "Sat 15:00",
-#  1382194800 => "Sat 18:00",
-#  1382205600 => "Sat 21:00",
-  1382216400 => "Sun 00:00",
-#  1382227200 => "Sun 03:00",
-#  1382238000 => "Sun 06:00",
-#  1382248800 => "Sun 09:00",
-  1382259600 => "Sun 12:00",
-#  1382270400 => "Sun 15:00",
-#  1382281200 => "Sun 18:00",
-#  1382292000 => "Sun 21:00",
-  1382302800 => "Mon 00:00",
-}
+g.labels = TimeLabels
+g.marker_font_size = 13
+
 g.write(OutputDir + "/privmsgs.png")
